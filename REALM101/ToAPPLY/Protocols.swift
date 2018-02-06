625cc43bd03b4c359a896307da659be1e90e69cb
Francis <Yuweh@users.noreply.github.com>
2018-02-06 16:45:48 +0800

    updated

--- REALM101/ToAPPLY/Protocols.swift
+++ REALM101/ToAPPLY/Protocols.swift
@@ -31,6 +31,41 @@ extension LoginViewController: SaveNewPinCodeUser {
     }
 }
 
-//****************************************** Protocols @2nd ViewController to QUERY and UPDATE ***********************************************************
+//****************************************** UPDATED VERSION ***********************************************************
 
-
+extension LoginViewController: SaveNewPinCodeUser {
+    func addNewUser(userNumber: String) {
+//        let newUser = PinUsers()
+//        newUser.userNumber = userNumber
+//        do {
+//            try GBARealm.write {
+//                GBARealm.add(newUser, update: true)
+//                //newUser.usersList2.append(newUser)
+//                //pinUsers.usersList.append(newUser)
+//                //processPin.users.append(newUser)
+//                //pinUsers[0].usersList.append(newUser)
+//            }
+//        } catch {
+//            print(error.localizedDescription)
+//        }
+        
+        do {
+            if let pinUser = GBARealm.object(ofType: PinUsers.self, forPrimaryKey: "\(userNumber)"){
+                //Nothing needs be done.
+                print(pinUser)
+                print("PinUser already existing")
+            } else {
+                //Create Person and dogs and relate them.
+                let newUser = PinUsers()
+                newUser.userNumber = userNumber
+                //newPerson.name = personName
+                
+                GBARealm.beginWrite()
+                GBARealm.create(PinUsers.self, value: newUser, update: true)
+                try GBARealm.commitWrite()
+                    }
+                } catch {
+            print("create and updating error")
+        }
+    }
+}
