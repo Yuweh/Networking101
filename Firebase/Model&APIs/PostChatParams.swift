916bd98068a013533d9d20537bee82bad6ef463e
Jay <Yuweh@users.noreply.github.com>
2020-03-12 15:27:09 +0800

    updated

---
+++ Firebase/Model&APIs/PostChatParams.swift
@@ -0,0 +1,14 @@
+//
+//  PostChatParams.swift
+//  Chat
+//
+//  Created by Francis Jemuel Bergonia on 3/9/20.
+//  Copyright © 2020 Xi Apps. All rights reserved.
+//
+
+import Foundation
+
+struct PostChatParams {
+    var name: String
+    var creatorId: String
+}
