14a9cd6269ef31a50a34a6965323bc0ea2261f4e
Jay <Yuweh@users.noreply.github.com>
2020-07-09 20:53:43 +0800

    updated

---
+++ Firebase/FireStore/Parsing.swift
@@ -0,0 +1,53 @@
+   //Variables
+    var categories = [Category]()
+    var selectedCategory: Category!
+    var firestoreDB: Firestore!
+    var listener: ListenerRegistration!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        firestoreDB = Firestore.firestore()
+
+
+fileprivate func fetchCollection() {
+        let collectionReference = self.firestoreDB.collection("categories")
+        listener = collectionReference.addSnapshotListener({ (snap, error) in
+            self.categories.removeAll()
+            guard let documents = snap?.documents else { return }
+            for document in documents {
+                let data = document.data()
+                let newCategory = Category.init(data: data)
+                self.categories.append(newCategory)
+            }
+            self.collectionView.reloadData()
+        })
+        
+//        collectionReference.getDocuments { (snap, error) in
+//            guard let documents = snap?.documents else { return }
+//            for document in documents {
+//                let data = document.data()
+//                let newCategory = Category.init(data: data)
+//                self.categories.append(newCategory)
+//            }
+//            self.collectionView.reloadData()
+//        }
+    }
+    
+    func fetchDocument() {
+        let docRef = self.firestoreDB.collection("categories").document("TQbCGRhPMGkaSI9Nbqtq")
+        
+        docRef.addSnapshotListener { (snap, error) in
+            self.categories.removeAll()
+            guard let data = snap?.data() else { return }
+            let newCategory = Category.init(data: data)
+            self.categories.append(newCategory)
+            self.collectionView.reloadData()
+        }
+        
+//        docRef.getDocument { (snap, error) in
+//            guard let data = snap?.data() else { return }
+//            let newCategory = Category.init(data: data)
+//            self.categories.append(newCategory)
+//            self.collectionView.reloadData()
+//        }
+    }
